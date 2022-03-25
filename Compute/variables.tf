f55c6c6606c08f22e2a64f93b983154f47415e3f
swayammishra95 <97121348+swayammishra95@users.noreply.github.com>
2022-03-25 21:42:39 +0530

    Add files via upload

---
+++ Compute/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_count" {}
+variable "instance_type" {}
+variable "public_sg" {}
+variable "public_subnets" {}
+variable "volume_size" {}
+variable "lb_target_group_arn" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "user_data_path" {}
