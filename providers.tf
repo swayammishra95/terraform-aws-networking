ca0e0f4d8a4395b4f466b71e48c25875018c9714
swayammishra95 <97121348+swayammishra95@users.noreply.github.com>
2022-11-25 18:02:06 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,16 +1,9 @@
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 
 
 # Configure the AWS Provider
 provider "aws" {
+  version = "~> 4.0"
   region = var.aws_region
   access_key = var.my-access-key
   secret_key = var.my-secret-key
